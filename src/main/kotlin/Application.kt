43408739b09a8ba30a75d6813094ec125da2fdb6
bruno.belloti <bruno.belloti@ecocentauro.com.br>
2025-06-20 09:18:04 -0300

    setup janus

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,13 +1,24 @@
 package com.class_erp
 
+
 import DatabaseConfig.appClient
 import DatabaseConfig.appMain
 import com.class_erp.schemas.AccessService
+import com.class_erp.service.JanusService
 import configureSockets
 import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.application.*
-import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.server.plugins.contentnegotiation.ContentNegotiation // ESTE É O ContentNegotiation DO SERVIDOR
 import org.koin.ktor.ext.inject
+import io.ktor.client.*
+import io.ktor.client.engine.cio.* // ou outro engine que você esteja usando
+
+// --- ADICIONE ESTE IMPORT PARA O PLUGIN DE CLIENTE COM ALIAS ---
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation as KtorClientContentNegotiation
+// --- FIM DO NOVO IMPORT ---
+
+import kotlinx.serialization.json.Json
+import org.koin.dsl.module
 import org.koin.ktor.plugin.Koin
 import org.koin.logger.slf4jLogger
 import routes.accessRouting
@@ -37,10 +48,35 @@ private fun Application.configureContentNegotiation() {
     }
 }
 
+private val janusModule = module {
+    single {
+        val appConfig = get<Application>().environment.config
+        appConfig.property("janus.baseUrl").getString()
+    }
+
+    single {
+        HttpClient(CIO) {
+            install(KtorClientContentNegotiation) {
+                json(Json {
+                    ignoreUnknownKeys = true
+                    prettyPrint = true
+                    isLenient = true
+                })
+            }
+            engine {
+                requestTimeout = 30000
+            }
+        }
+    }
+    single {
+        JanusService(get<HttpClient>(), get<String>())
+    }
+}
+
 private fun Application.configureDependencyInjection() {
     install(Koin) {
         slf4jLogger()
-        modules(appMain, appClient)
+        modules(appMain, appClient,janusModule)
     }
 }
 
@@ -49,6 +85,7 @@ private fun Application.configureRouting() {
     val classesListService by inject<ClassesListService>()
     val uploadListService by inject<UploadService>()
     val clientService: ClientService by inject<ClientService>()
+    val janusService by inject<JanusService>()
 
     clientRouting(clientService)
     accessRouting(serviceAccess)
