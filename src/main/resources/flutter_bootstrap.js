184f803ea5a4abd8a527f0e59d68f3895a092f18
bruno.belloti <bruno.belloti@ecocentauro.com.br>
2025-11-18 09:33:51 -0300

    otimiza imagens

--- src/main/resources/flutter_bootstrap.js
+++ src/main/resources/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "550811137"
+    serviceWorkerVersion: "2301602342"
   }
 });
