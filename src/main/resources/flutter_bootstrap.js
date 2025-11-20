eb0d75c2e180847666fc61031fb10d14868cf42a
bruno.belloti <bruno.belloti@ecocentauro.com.br>
2025-11-20 16:30:39 -0300

    version prod com categorias

--- src/main/resources/flutter_bootstrap.js
+++ src/main/resources/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2078950099"
+    serviceWorkerVersion: "355689059"
   }
 });
