6b77d5d28dbcf94a6deeda62df1c7870e9ab5b9c
bruno.belloti <bruno.belloti@ecocentauro.com.br>
2025-12-11 15:20:17 -0300

    versao prod new logo

--- src/main/resources/flutter_bootstrap.js
+++ src/main/resources/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "724967366"
+    serviceWorkerVersion: "4277823811"
   }
 });
